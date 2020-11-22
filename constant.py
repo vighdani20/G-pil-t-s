0975a43fe900126da1b02bd5e5fbda7fc10e8c53
vighdani20 <71558341+vighdani20@users.noreply.github.com>
2020-11-22 20:43:12 +0100

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,8 @@
+width_min = 80  # Minimum rectangle width // Minimalis teglalap szelesseg
+height_min = 80  # Minimum rectangle height // Minimalis teglalap magassag
+offset = 6  # Allowed error between pixels // keppont megengedettseg
+pos_line = 550  # Line positional // Vonal pozicio
+delay = 60  # Video FPS
+detect = []
+cars= 0
+
